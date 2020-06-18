40257ef5dd394d54f3b58847bee4dc3c5ece8574
Noccz <radiation_night@hotmail.com>
2020-06-18 23:27:07 +0200

    Change item layout, add delete query and add status updates on items
    - Changed item layout to CardView.
    - Added delete query which is done by long click on item and selecting
    delete.
    - Added the final status updates which allows the user to see and set
    which tasks are done and also undo tasks.
    - Card items gets visually updated to red if the set time has been passed
    and the task is not done.

--- app/src/main/java/com/noccz/invasive_routine/routine/RoutineViewAdapter.java
+++ app/src/main/java/com/noccz/invasive_routine/routine/RoutineViewAdapter.java
@@ -1,21 +1,28 @@
 package com.noccz.invasive_routine.routine;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.noccz.invasive_routine.R;
+import com.noccz.invasive_routine.database.DatabaseManager;
 import com.noccz.invasive_routine.task.TaskItem;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.List;
 
+import static com.noccz.invasive_routine.Utils.findViewById;
 import static com.noccz.invasive_routine.Utils.getGsonParser;
 
 public class RoutineViewAdapter extends RecyclerView.Adapter {
@@ -54,17 +61,55 @@ public class RoutineViewAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         RoutineItemHolder routineItemHolder = (RoutineItemHolder) holder;
-        final int pos = position;
+        final TaskItem taskItem = taskItems.get(position);
         routineItemHolder.bind(taskItems.get(position));
+        if (taskItem.isCompleted()) {
+            ((CardView)routineItemHolder.itemView).setCardBackgroundColor(routineView.getResources().getColor(R.color.colorComplete, null));
+        } else {
+            Calendar calendar = Calendar.getInstance();
+            if (calendar.get(Calendar.HOUR_OF_DAY) > taskItem.getHour() ||
+                    calendar.get(Calendar.HOUR_OF_DAY) == taskItem.getHour() && calendar.get(Calendar.MINUTE) > taskItem.getMinute()) {
+                ((CardView)routineItemHolder.itemView).setCardBackgroundColor(routineView.getResources().getColor(R.color.colorNotComplete, null));
+            }
+        }
         routineItemHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString("taskItem", getGsonParser().toJson(taskItems.get(pos)));
+                bundle.putString("taskItem", getGsonParser().toJson(taskItem));
 
                 NavHostFragment.findNavController(routineView)
                         .navigate(R.id.action_RoutineView_to_TaskManager, bundle);
-                view.setVisibility(View.INVISIBLE);
+                findViewById(view.getRootView(), R.id.add_button).setVisibility(View.INVISIBLE);
+            }
+        });
+        routineItemHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(final View view) {
+                PopupMenu popup = new PopupMenu(view.getRootView().getContext(),view, Gravity.END, R.attr.actionOverflowMenuStyle, 0);
+                popup.inflate(R.menu.menu_task_item);
+                if (taskItem.isCompleted()) {
+                    popup.getMenu().getItem(0).setTitle(R.string.undone);
+                } else {
+                    popup.getMenu().getItem(0).setTitle(R.string.done);
+                }
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_delete:
+                                deleteTaskItem(taskItem);
+                                return true;
+                            case R.id.action_task_status:
+                                updateTaskStatus(taskItem);
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popup.show();
+                return true;
             }
         });
     }
@@ -72,5 +117,38 @@ public class RoutineViewAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return taskItems.size();
+    }
+
+    private void deleteTaskItem(TaskItem item) {
+        DatabaseManager dbManager = new DatabaseManager(routineView.getContext());
+        dbManager.open();
+        dbManager.delete(item.getId());
+        dbManager.close();
+        notifyDataSetChanged();
+        refreshView();
+    }
+
+    private void updateTaskStatus(TaskItem taskItem) {
+        DatabaseManager dbManager = new DatabaseManager(routineView.getContext());
+        dbManager.open();
+        if (!taskItem.isCompleted()) {
+            dbManager.update(taskItem.newBuilderWithCurrent()
+                    .withIsCompleted(1)
+                    .build());
+        } else {
+            dbManager.update(taskItem.newBuilderWithCurrent()
+                    .withIsCompleted(0)
+                    .build());
+        }
+        dbManager.close();
+        refreshView();
+    }
+
+    private void refreshView() {
+        notifyDataSetChanged();
+        routineView.getParentFragmentManager().beginTransaction()
+                .detach(routineView)
+                .attach(routineView)
+                .commit();
     }
 }
